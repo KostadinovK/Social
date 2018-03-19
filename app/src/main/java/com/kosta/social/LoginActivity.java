e2813cad14501be1ca94bc9ab4642ccdb547a0d8
KostadinovK <kostadinovk2001@gmail.com>
2018-03-19 21:07:28 +0200

    Finished login screen

--- app/src/main/java/com/kosta/social/LoginActivity.java
+++ app/src/main/java/com/kosta/social/LoginActivity.java
@@ -1,15 +1,31 @@
 package com.kosta.social;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AdditionalUserInfo;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
     @BindView(R.id.edt_email)
     EditText mEmail;
     @BindView(R.id.edt_password)
@@ -24,17 +40,63 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
+
+
     @OnClick(R.id.btn_login)
-    public void onLoginClicked(){
+    public void onLoginClicked() {
+        String email = mEmail.getText().toString();
+        String password = mPassword.getText().toString();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
 
-
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Authentication successful.",
+                                    Toast.LENGTH_SHORT).show();
+                            redirectToMainActivity();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     @OnClick(R.id.btn_register)
-    public void onRegisterClicked(){
+    public void onRegisterClicked() {
+        String email = mEmail.getText().toString();
+        String password = mPassword.getText().toString();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Authentication successful.",
+                                    Toast.LENGTH_SHORT).show();
+                            redirectToMainActivity();
+                        } else {
+
+                            Toast.makeText(LoginActivity.this, "Authentication failed." ,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
 
+
+    }
+
+
+    public void redirectToMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
